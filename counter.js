081b6aca146b10642cd75cad8122bf6ce78c64a4
Lamim <lemon07r@gmail.com>
2019-06-05 18:42:54 -0400

    Initial commit of prototype

---
+++ counter.js
@@ -0,0 +1,23 @@
+let pressCount = 0;
+let duration = 0;
+
+function counterUpdate() {
+    let cpm = (pressCount / (duration / 10)) * 60;
+    let wpm = cpm / 5;
+
+    document.getElementById("counter").innerText = wpm.toFixed(1);
+}
+
+window.setInterval(() => {
+    duration++;
+    counterUpdate();
+}, 100);
+
+document.addEventListener("keypress", logKey);
+
+function logKey() {
+    pressCount++;
+    setTimeout(function () {
+        pressCount--;
+    }, 10000);
+}
